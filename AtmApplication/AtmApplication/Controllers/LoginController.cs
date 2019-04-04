f506fcdd05c10fd02d5e19b3269690e524c80cb5
yolanda <ysimpson@spartaglobal.com>
2019-04-04 16:20:15 +0100

    version 1

---
+++ AtmApplication/AtmApplication/Controllers/LoginController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AtmApplication;
+
+namespace AtmApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        private ATMDBEntities db = new ATMDBEntities();
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: Login/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Login/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Login/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName_,LastName,TelephoneNumber,AddressLine1,AddressLine2,AddressLine3,CustomerNumber,Pin")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        //POST: Login
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            User usr = db.Users.FirstOrDefault(u => u.CustomerNumber == (user.CustomerNumber) && u.Pin == (user.Pin));
+            if (usr == null || usr.Pin == null)
+            {
+                return RedirectToAction("Create", "Users");
+            }
+            
+
+                //user directed to own details 
+                return View("Transactions", usr);
+
+          
+        }
+
+        // GET: Login/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Login/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName_,LastName,TelephoneNumber,AddressLine1,AddressLine2,AddressLine3,CustomerNumber,Pin")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Login/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Login/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AtmApplication/AtmApplication/Controllers/WithdrawalsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AtmApplication;
+
+namespace AtmApplication.Controllers
+{
+    public class WithdrawalsController : Controller
+    {
+        private ATMDBEntities db = new ATMDBEntities();
+
+        // GET: Withdrawals
+        public ActionResult Index()
+        {
+            return View(db.Withdrawals.ToList());
+        }
+
+        // GET: Withdrawals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Withdrawal withdrawal = db.Withdrawals.Find(id);
+            if (withdrawal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(withdrawal);
+        }
+
+        // GET: Withdrawals/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Withdrawals/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,WithdrawalAmount,WithdrawalDate")] Withdrawal withdrawal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Withdrawals.Add(withdrawal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(withdrawal);
+        }
+
+        // GET: Withdrawals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Withdrawal withdrawal = db.Withdrawals.Find(id);
+            if (withdrawal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(withdrawal);
+        }
+
+        // POST: Withdrawals/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,WithdrawalAmount,WithdrawalDate")] Withdrawal withdrawal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(withdrawal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(withdrawal);
+        }
+
+        // GET: Withdrawals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Withdrawal withdrawal = db.Withdrawals.Find(id);
+            if (withdrawal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(withdrawal);
+        }
+
+        // POST: Withdrawals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Withdrawal withdrawal = db.Withdrawals.Find(id);
+            db.Withdrawals.Remove(withdrawal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
